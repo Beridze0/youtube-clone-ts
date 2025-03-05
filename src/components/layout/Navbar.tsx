df714f338eeca0564608b3c8c5a6caaf16bb9647
Beridze0 <beridzedavit904@gmail.com>
2025-03-05 05:21:44 -0800

    finished dialog

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -10,38 +10,55 @@ import { Input } from "../ui/input";
 const Navbar = () => {
   return (
     <div className="flex items-center justify-between border-b border-b-border px-5">
-        <div>
-            <h1>VIDEOWEB</h1>
-        </div>
-        <Dialog>
+      <div>
+        <h1>VIDEOWEB</h1>
+      </div>
+      <Dialog>
         <DialogTrigger>
-            <div className="cursor-pointer w-[30rem] border border-border outline outline-none shadow-none rounded-md hover:bg-secondary ">
-                <Input className="border-none " placeholder="Search..." disabled />
-            </div>
+          <div className="cursor-pointer w-[30rem] border border-border outline outline-none shadow-none rounded-md hover:bg-secondary ">
+            <Input className="border-none " placeholder="Search..." disabled />
+          </div>
         </DialogTrigger>
         <DialogContent className="p-0 py-1.5">
-            <div className="flex items-center w-full border border-transparent border-b-border">
-                
+          <div className="flex items-center w-full border border-transparent border-b-border">
             <Search size={18} className="ml-2.5" />
-            <Input placeholder="Search for a video..." className="flex items-center rounded-none border-transparent  shadow-none w-full" />
+            <Input
+              placeholder="Search for a video..."
+              className="flex items-center rounded-none border-transparent  shadow-none w-full"
+            />
+          </div>
+
+          <DialogDescription className="px-3" style={{ fontSize: "0.8rem" }}>
+            Enter keywords related to the video you're looking for. You can
+            search by title, category, or creator name.
+          </DialogDescription>
+          <div className="px-3 pb-2 flex flex-col">
+            <h1 className="mb-1 text-[1.05rem]">Search history</h1>
+            <div className="text-sm flex flex-col gap-1">
+              <p className="text-muted-foreground transition-all cursor-pointer duration-200 hover:text-primary">Top 10 tech gadgets 2024</p>
+              <p className="text-muted-foreground transition-all cursor-pointer duration-200 hover:text-primary">
+                How to edit videos in Premiere Pro
+              </p>
+              <p className="text-muted-foreground transition-all cursor-pointer duration-200 hover:text-primary">
+                Best lo-fi music for studying
+              </p>
+              <p className="text-muted-foreground transition-all cursor-pointer duration-200 hover:text-primary">
+                CS2 gameplay tips and tricks
+              </p>
+              <p className="text-muted-foreground transition-all cursor-pointer duration-200 hover:text-primary">
+                Marvel movie timeline in order
+              </p>
             </div>
-            
-            <DialogDescription className="px-3">
-                Enter keywords related to the video you're looking for. You can
-                search by title, category, or creator name.
-            </DialogDescription>
-            <div>
-                <p></p>
-            </div>
+          </div>
         </DialogContent>
-        </Dialog>
+      </Dialog>
+      <div>
+        <EllipsisVertical />
         <div>
-            <EllipsisVertical />
-            <div>
-                <CircleUserRound />
-                <p>Sign in</p>
-            </div>
+          <CircleUserRound />
+          <p>Sign in</p>
         </div>
+      </div>
     </div>
   );
 };
