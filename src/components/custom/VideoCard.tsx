ce58faa85d1070c04680087a5c635ec9b05fdf7b
Beridze0 <beridzedavit904@gmail.com>
2025-03-14 04:30:33 -0700

    cardlist

--- src/components/custom/VideoCard.tsx
+++ src/components/custom/VideoCard.tsx
@@ -2,16 +2,15 @@ import { Card, CardContent } from "@/components/ui/card";
 import example from "@/assets/example.jpg";
 const VideoCard = () => {
   return (
-    <Card className="p-0 rounded-none border shadow-none">
+    <Card className="p-0 rounded-none border-none bg-transparent shadow-none">
       <CardContent className="p-0 ">
-        <div>
-          <img src={example} alt="Image" className="w-[310px] h-[180px]" />
+        <div className="w-[310px] h-[180px]">
+          <img src={example} alt="Image" className="w-full h-full rounded-md" />
         </div>
-        <div>
-            <p>Learn React for Beginners</p>
-            <p>1.5M views</p>
-            <p>Uploaded March 12, 2025</p>
-            <p>React Mastery</p>
+        <div className="px-2 mt-1.5">
+            <p className="font-semibold">Learn React for Beginners</p>
+            <p className="text-[0.79rem]">1.5M views &bull; March 12, 2025</p>
+            <p className="text-[0.79rem]">React Mastery</p>
         </div>
       </CardContent>
     </Card>
