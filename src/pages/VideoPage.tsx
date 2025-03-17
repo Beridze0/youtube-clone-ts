3677eedf25756777065c2e963bccd63c34dc5aa8
Beridze0 <beridzedavit904@gmail.com>
2025-03-17 09:34:32 -0700

    video

--- src/pages/VideoPage.tsx
+++ src/pages/VideoPage.tsx
@@ -1,8 +1,12 @@
+import example from "@/assets/example.jpg"
 
 const VideoPage = () => {
   return (
     <div>
-        
+        {/* left side */}
+        <div className="w-[750px] h-[450px]">
+            <img src={example} alt="" className="w-full h-full object-cover" />
+        </div>
     </div>
   )
 }
