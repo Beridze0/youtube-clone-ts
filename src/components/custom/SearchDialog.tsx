e45e8a250d7f6351f366e10bb86a33285621677c
Beridze0 <beridzedavit904@gmail.com>
2025-03-19 08:04:02 -0700

    redux

--- src/components/custom/SearchDialog.tsx
+++ src/components/custom/SearchDialog.tsx
@@ -1,7 +1,28 @@
-import { Dialog, DialogContent, DialogTrigger } from "@/components/ui/dialog";
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { fetchSearchResults, setSearchTerm } from "@/redux/searchSlice";
+import { AppDispatch } from "@/redux/store";
 import { History } from "lucide-react";
+import { useState } from "react";
+import { useDispatch } from "react-redux";
 
 const SearchDialog = () => {
+  const [input, setInput] = useState("");
+  const dispatch = useDispatch<AppDispatch>();
+
+  const handleSearch = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    if (input.trim() === "") return;
+    dispatch(setSearchTerm(input));
+    dispatch(fetchSearchResults(input));
+    setInput("")
+  };
+
   return (
     <Dialog>
       <DialogTrigger>
@@ -10,13 +31,19 @@ const SearchDialog = () => {
         </div>
       </DialogTrigger>
       <DialogContent className="m-0 outline-none px-1 py-3 gap-3 flex flex-col">
-        <div className="border-b w-full">
+        <DialogHeader>
+          <DialogTitle />
+        <form onSubmit={(e) => handleSearch(e)} className="border-b w-full">
           <input
             type="text"
             placeholder="Search for video..."
             className="text-sm pb-2 px-2 w-full outline-none"
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
           />
-        </div>
+        </form>
+        </DialogHeader>
+
         <div className="px-2">
           <div className="flex items-center gap-1 mb-1">
             <p className="text-[0.96rem] mb-0.5">Search history</p>
