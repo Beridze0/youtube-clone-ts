e1c89027b8eaf290929ffb980fd26958b85f86d8
Beridze0 <beridzedavit904@gmail.com>
2025-03-26 09:29:20 -0700

    del

--- src/pages/SearchPage.tsx
+++ src/pages/SearchPage.tsx
@@ -2,26 +2,24 @@ import { useDispatch } from "react-redux";
 import { useEffect } from "react";
 import { useParams } from "react-router-dom";
 import VideoList from "@/components/custom/VideoList";
-import { addSearchQuery } from "@/redux/searchSlice"; // Action to set query in state
+import { addSearchQuery } from "@/redux/searchSlice"; 
 
 const SearchPage = () => {
   const { query } = useParams();
   const dispatch = useDispatch();
 
   useEffect(() => {
-    // Check if the query is valid and not empty
     if (query) {
-      // Retrieve existing search history from localStorage
-      const existingSearchHistory = JSON.parse(localStorage.getItem('search-history') || '[]');
+      const existingSearchHistory = JSON.parse(
+        localStorage.getItem("search-history") || "[]"
+      );
 
-      // If the query isn't already in the history, add it
       if (!existingSearchHistory.includes(query)) {
         existingSearchHistory.push(query);
-
-        // Save the updated search history to localStorage
-        localStorage.setItem('search-history', JSON.stringify(existingSearchHistory));
-
-        // Dispatch the action to update Redux state
+        localStorage.setItem(
+          "search-history",
+          JSON.stringify(existingSearchHistory)
+        );
         dispatch(addSearchQuery(query));
       }
     }
